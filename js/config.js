b96a8839ab5ed3f1ae5f9410426411f95f259ea8
Regis Mendes <regisszmendes@gmail.com>
2026-04-29 09:40:26 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 // ============================================================
 
 const SUPABASE_URL = "https://fskbraltrjhxnvlfshqb.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZza2JyYWx0cmpoeG52bGZzaHFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzcwNTM0NjcsImV4cCI6MjA1MjYyOTQ2N30.sb_publishable_LwV8tmjClKkcVQRvxtUUpg_IH8JkHLc";
+const SUPABASE_KEY = "sb_publishable_LwV8tmjClKkcVQRvxtUUpg_IH8JkHLc";
 
 window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
